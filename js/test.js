bc4a758f9b5ed5f6a940435648984cb2f0e29240
Lucas Cini <lucas.cini10@gmail.com>
2017-09-13 10:58:38 +0200

    add character

--- js/test.js
+++ js/test.js
@@ -9,7 +9,7 @@ var ctx = canvas.getContext('2d');
 ctx.canvas.width  = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
-//Dégradé
+//  Dégradé
 ctx.beginPath();
 var degrade = ctx.createLinearGradient(0, screenHeight, 0, 0);
 degrade.addColorStop(0, '#113F9B');
@@ -19,30 +19,52 @@ ctx.fillStyle = degrade;
 ctx.fillRect(0, 0, screenWidth, screenHeight);
 ctx.closePath;
 
-//Spawn
+//  Spawn
 ctx.beginPath();
+var spawnWidth = 100;
+var spawnHeight = 30;
 var spawnx = 0;
-var spawny = screenHeight -30;
-ctx.rect(spawnx, spawny, 100, 30);
+var spawny = screenHeight - spawnHeight;
+ctx.rect(spawnx, spawny, spawnWidth, spawnHeight);
 ctx.fillStyle = "#fff";
 ctx.fill();
 ctx.closePath;
 
-//Ending
-var endingx = screenWidth - 100;
-var endingy = screenHeight -30;
-ctx.rect(endingx, endingy, 100, 30);
-ctx.fillStyle = "#fff";
-ctx.fill();
-ctx.closePath;
-
-//Floor
+//  Ending
 ctx.beginPath();
+var endingWidth = 100;
+var endingHeight = 30;
+var endingx = screenWidth - endingWidth;
+var endingy = screenHeight - endingHeight;
+ctx.rect(endingx, endingy, endingWidth, endingHeight);
+ctx.fillStyle = "#fff";
+ctx.fill();
+ctx.closePath;
+
+//  Floor
+
 var floorWidth = screenWidth - 200;
 var floorHeight = 30;
 var floorx = 100;
-var floory = screenHeight -30;
+var floory = screenHeight - 30;
+ctx.beginPath();
 ctx.rect(floorx, floory,floorWidth, floorHeight);
 ctx.fillStyle = "#84D6A3"
 ctx.fill()
 ctx.closePath;
+
+//  Character
+var charaWidth = 20;
+var charaHeight = 60;
+var charax = 30;
+var charay = floory - charaHeight;
+
+function drawCharacter() {
+    ctx.beginPath();
+    ctx.rect(charax, charay, charaWidth, charaHeight);
+    ctx.fillStyle = "#74B8D1";
+    ctx.fill();
+    ctx.closePath;
+}
+
+drawCharacter()
